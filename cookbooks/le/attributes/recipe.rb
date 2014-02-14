fb90b2d34f71955f216b5e03f7175843e02b9ce3
Hugh Kelsey <hugh@45robots.com>
2014-02-14 06:20:01 -0800

    added le

---
+++ cookbooks/le/attributes/recipe.rb
@@ -0,0 +1,2 @@
+le_api_key('057586d2-2cf3-4506-a1a5-0b7223219f1c')  
+# Read more at https://logentries.com/doc/engineyard/
---
+++ cookbooks/le/recipes/configure.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: le
+# Recipe:: configure
+#
+
+execute "le register --account-key" do
+  command "le register --account-key #{node[:le_api_key]} --name #{node[:applications].keys.first}"
+  action :run
+  not_if { File.exists?('/etc/le/config') } 
+end
+
+follow_paths = [
+  "/var/log/syslog",
+  "/var/log/auth.log",
+  "/var/log/daemon.log"
+]
+(node[:applications] || []).each do |app_name, app_info|
+  follow_paths << "/var/log/nginx/#{app_name}.access.log"
+  follow_paths << "/var/log/nginx/#{app_name}.error.log"
+  follow_paths << "/data/teamconnect/current/log/staging.log"    # Production log
+end
+
+follow_paths.each do |path|
+  execute "le follow #{path}" do
+    command "le follow #{path}"
+    ignore_failure true 
+    action :run
+  end
+end
